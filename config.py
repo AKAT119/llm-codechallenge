035f4dd5a717fb1f134d435645f3b1b0bd5088c6
AzinKatiraee <53387554+AKAT119@users.noreply.github.com>
2024-09-07 23:52:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 # Set OpenAI API key here or use environment variables
 OPENAI_API_KEY = os.getenv(
-    "OPENAI_API_KEY", "sk-None-yobuul67W7Cp4VrswxJtT3BlbkFJnY4vGo6D8toa3IdqaQbc"
+    "OPENAI_API_KEY", "sk-*"  # request me for API Key
 )
